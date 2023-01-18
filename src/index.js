b34ce32f30af376e2daf0cbe8e70e9b386ea6bc2
sapsu <113932803+sapsu@users.noreply.github.com>
2023-01-18 12:45:19 +0100

    Added js for displaying Helsinki time and date

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-
+function updateTime() {
+  let HelsinkiCurrentTime = document.querySelector("#helsinki .time");
+  let HelsinkiCurrentDate = document.querySelector("#helsinki .date");
+  HelsinkiCurrentTime.innerHTML = moment()
+    .tz("Europe/Helsinki")
+    .format("h:mm:ss A");
+  HelsinkiCurrentDate.innerHTML = moment()
+    .tz("Europe/Helsinki")
+    .format("MMMM D, YYYY");
+}
+updateTime();
+setInterval(updateTime, 1000);
